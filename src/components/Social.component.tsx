8f2bd47113b26d2b82b79d1d1d614fa4b332004f
Mark Cahill <mark@thinkjson.com>
2020-04-25 11:57:32 -0400

    Add fallback for social

--- src/components/Social.component.tsx
+++ src/components/Social.component.tsx
@@ -4,6 +4,61 @@ interface SocialState {
     loaded: boolean;
 }
 
+const SocialLoaded = () => (
+    <React.Fragment>
+        <div className="row">
+            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <a
+                    className="twitter-timeline"
+                    data-height="400"
+                    href="https://twitter.com/firstdeland?ref_src=twsrc%5Etfw"
+                >
+                    Tweets by firstdeland
+                </a>
+            </div>
+        
+            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <div
+                    className="fb-page"
+                    data-href="https://www.facebook.com/firstassemblydeland"
+                    data-tabs="timeline"
+                    data-height="400"
+                    data-width="1000"
+                    data-small-header="true"
+                    data-adapt-container-width="true"
+                    data-hide-cover="false"
+                    data-show-facepile="false"
+                >
+                    <blockquote
+                        cite="https://www.facebook.com/firstassemblydeland"
+                        className="fb-xfbml-parse-ignore"
+                    >
+                        <a href="https://www.facebook.com/firstassemblydeland">
+                            First Assembly DeLand
+                        </a>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div id="fb-root"></div>
+    </React.Fragment>
+);
+
+const SocialNotLoaded = () => (
+    <div className="social row">
+        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div>
+                <a href="https://twitter.com/firstdeland" target="_blank"><h3><i className="icon-twitter"></i> Twitter</h3></a>
+            </div>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            <div>
+                <a href="https://www.facebook.com/firstassemblydeland" target="_blank"><h3><i className="icon-facebook2"></i> Facebook</h3></a>
+            </div>
+        </div>    
+    </div>
+);
+
 export class Social extends React.Component<{}, SocialState> {
     constructor(props: {}) {
         super(props);
@@ -29,48 +84,10 @@ export class Social extends React.Component<{}, SocialState> {
     }
 
     render() {
-        if (!this.state.loaded) {
-            return false;
-        }
         return (
             <React.Fragment>
                 <h2>Connect with us</h2>
-
-                <div className="row">
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                        <a
-                            className="twitter-timeline"
-                            data-height="400"
-                            href="https://twitter.com/firstdeland?ref_src=twsrc%5Etfw"
-                        >
-                            Tweets by firstdeland
-                        </a>
-                    </div>
-                
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                        <div
-                            className="fb-page"
-                            data-href="https://www.facebook.com/firstassemblydeland"
-                            data-tabs="timeline"
-                            data-height="400"
-                            data-width="1000"
-                            data-small-header="true"
-                            data-adapt-container-width="true"
-                            data-hide-cover="false"
-                            data-show-facepile="false"
-                        >
-                            <blockquote
-                                cite="https://www.facebook.com/firstassemblydeland"
-                                className="fb-xfbml-parse-ignore"
-                            >
-                                <a href="https://www.facebook.com/firstassemblydeland">
-                                    First Assembly DeLand
-                                </a>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-                <div id="fb-root"></div>
+                {this.state.loaded ? <SocialLoaded /> : <SocialNotLoaded />}
             </React.Fragment>
         );
     }
